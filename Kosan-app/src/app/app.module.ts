81051ddc5a605e53d537eaa72d3306f85255446e
Arief <arief.riski11@gmail.com>
2022-12-29 19:59:13 +0700

    Add Routing Page For Component

--- Kosan-app/src/app/app.module.ts
+++ Kosan-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -7,6 +8,27 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { PropertyListComponent } from './property/property-list/property-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HousingService } from './services/housing.service';
+import { AddPropertyComponent } from './property/add-property/add-property.component';
+import { PropertyDetailComponent } from './property/property-detail/property-detail.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: PropertyListComponent,
+  },
+  {
+    path: 'add-property',
+    component: AddPropertyComponent,
+  },
+  {
+    path: 'rent-property',
+    component: PropertyListComponent,
+  },
+  {
+    path: 'property-detail/:id',
+    component: PropertyDetailComponent,
+  },
+];
 
 @NgModule({
   declarations: [
@@ -14,8 +36,10 @@ import { HousingService } from './services/housing.service';
     PropertyCardComponent,
     NavbarComponent,
     PropertyListComponent,
+    AddPropertyComponent,
+    PropertyDetailComponent,
   ],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
   providers: [HousingService],
   bootstrap: [AppComponent],
 })
--- Kosan-app/src/app/property/property-list/property-list.component.ts
+++ Kosan-app/src/app/property/property-list/property-list.component.ts
@@ -1,23 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { HousingService } from 'src/app/services/housing.service';
 import { IProperty } from '../IProperty.interface';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css'],
 })
 export class PropertyListComponent implements OnInit {
-  properties: Array<IProperty>;
-
-  constructor(private housingService: HousingService) {
+  properties: IProperty[];
+  SellRent = 1;
+  constructor(
+    private route: ActivatedRoute,
+    private housingService: HousingService
+  ) {
     this.properties = [];
   }
 
   ngOnInit(): void {
-    this.housingService.getAllProperties().subscribe(
+    if (this.route.snapshot.url.toString()) {
+      this.SellRent = 2;
+    }
+    this.housingService.getAllProperties(this.SellRent).subscribe(
       (data) => {
         this.properties = data;
+        console.log(this.route.snapshot.url.toString());
       },
       (error) => {
         console.log(error);
